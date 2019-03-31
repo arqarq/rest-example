2d16a9ea6ec6fa03917a24310b744273b4ff6068
arqarq <43341071+arqarq@users.noreply.github.com>
2019-03-31 09:54:15 +0200

    - validation example updated

--- security-example/src/main/java/pl/sdacademy/securityexample/LoginController.java
+++ security-example/src/main/java/pl/sdacademy/securityexample/LoginController.java
@@ -1,10 +1,12 @@
 package pl.sdacademy.securityexample;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
+    @CrossOrigin
     @PostMapping("/login")
     public void login() {
 
